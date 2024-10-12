a0cd35ee612aaa233d2ddc16966cca9398b07efd
vv-meow-meow <181574907+vv-meow-meow@users.noreply.github.com>
2024-10-12 21:34:56 +0300

    Remove num2words package, create own logic

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
 import re
 import logging
 
-from num2words import num2words
+from numbers import parse_number_word, parse_word_number
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
-
-def generate_numbers(min_num: int, max_num: int) -> dict[str, int]:
-    """
-    Generate a dictionary with numbers between min and max
-    :param min_num: minimal number
-    :param max_num: maximal number
-    :return: a dictionary with numbers between min and max
-    """
-    numbers: dict = {}
-    for i in range(min_num, max_num + 1):
-        word = num2words(i, lang='ru')
-        numbers[word] = i
-    return numbers
-
-
-numbers = generate_numbers(1, 100)
 
 
 def parse_and_calculate(equation: list = ("ноль", "плюс", "ноль")) -> str:
@@ -36,15 +19,15 @@ def parse_and_calculate(equation: list = ("ноль", "плюс", "ноль")) -
 
     match equation[1]:
         case "плюс":
-            result_number = numbers[equation[0]] + numbers[equation[2]]
+            result_number = parse_word_number(equation[0]) + parse_word_number(equation[2])
         case "минус":
-            result_number = numbers[equation[0]] - numbers[equation[2]]
+            result_number = parse_word_number(equation[0]) - parse_word_number(equation[2])
         case "умножить на":
-            result_number = numbers[equation[0]] * numbers[equation[2]]
+            result_number = parse_word_number(equation[0]) * parse_word_number(equation[2])
         case "разделить на":
-            result_number = numbers[equation[0]] / numbers[equation[2]]
+            result_number = parse_word_number(equation[0]) - parse_word_number(equation[2])
 
-    result = num2words(result_number, lang='ru')
+    result = parse_number_word(result_number)
     return result
 
 
@@ -52,7 +35,7 @@ def check_equation_string(equation: str) -> list | bool:
     """
     Check if the equation string is valid ("number operation number")
     :param equation: string of the equation
-    :return: list of valid equations / False if equation string is invalid
+    :return: list of valid equation / False if equation string is invalid
     """
     equation = re.split(r'(плюс|минус|умножить на|разделить на)', equation)
     equation = [element.strip() for element in equation]
@@ -66,7 +49,19 @@ def main() -> None:
     Main function of the program. Requests the user to input the equation and prints the result of the equation.
     :return: nothing
     """
-    equation = input("Здравствуйте, введите математическое выражение. [число оператор число]\n").lower()
+    print("------------------------------------------------\n"
+          "Здравствуйте! Небольшая сводка помощи перед началом использования.\n"
+          "------------------------------------------------\n"
+          "Q: Как писать выражение?\n"
+          "A: [число операция число]\n"
+          "------------------------------------------------\n"
+          "Операции:\n"
+          "- Сложение – плюс\n"
+          "- Вычитание – минус\n"
+          "- Умножение – умножить на\n"
+          "- Деление – разделить на\n"
+          "------------------------------------------------")
+    equation = input("Введите математическое выражение: ").lower()
 
     equation = check_equation_string(equation)
     while not equation:
---
+++ numbers/__init__.py
@@ -0,0 +1,4 @@
+from .numbers import parse_word_number, parse_number_word
+
+__all__ = ["parse_word_number",
+           "parse_number_word"]
