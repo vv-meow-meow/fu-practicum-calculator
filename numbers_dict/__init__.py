282e869b5891879f1c2024c435fdacdef4ec74eb
vv-meow-meow <181574907+vv-meow-meow@users.noreply.github.com>
2024-10-25 15:20:26 +0300

    Add parse_word_to_number

--- numbers_dict/__init__.py
+++ numbers_dict/__init__.py
@@ -1,4 +1,4 @@
-from .numbers_logic import parse_word_to_number, parse_number_word
+from .numbers_logic import parse_word_to_number, parse_number_to_word
 
 __all__ = ["parse_word_to_number",
-           "parse_number_word"]
+           "parse_number_to_word"]
