00b3aa6a9a7647184740368e3090aba8a30a4781
vv-meow-meow <181574907+vv-meow-meow@users.noreply.github.com>
2024-10-25 15:21:45 +0300

    Add test_parse_number_to_word()

--- tests/test_numbers_logic.py
+++ tests/test_numbers_logic.py
@@ -1,5 +1,5 @@
 import pytest
-from numbers_dict import parse_word_to_number
+from numbers_dict import *
 
 
 @pytest.mark.parametrize("input_value, expected_output", [
@@ -14,7 +14,7 @@ from numbers_dict import parse_word_to_number
     ("тысяча двести пятьдесят два", 1252),
     ("миллион", 1_000_000),
     ("пять миллионов", 5_000_000),
-    #     Negative
+    # Negative
     ("минус один", -1),
     ("минус сорок два", -42),
     ("минус тысяча сто один", -1101),
@@ -24,6 +24,22 @@ from numbers_dict import parse_word_to_number
                          )
 def test_parse_word_to_number(input_value, expected_output) -> None:
     result = parse_word_to_number(input_value)
+
+    # Assert
+    assert result == expected_output
+
+
+@pytest.mark.parametrize("input_value, expected_output", [
+    (0, 'ноль'),
+    (10, 'десять'),
+    (15, 'пятнадцать'),
+    (20, "двадцать"),
+    (25, "двадцать пять"),
+    # Negative tests: TODO
+    # (-10, "минус десять")
+])
+def test_parse_number_to_word(input_value, expected_output) -> None:
+    result = parse_number_to_word(input_value)
 
     # Assert
     assert result == expected_output
